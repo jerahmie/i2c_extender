467024dfa34ffa90d3e695127b523bf6bf931eb3
Jerahmie Radder <jerahmie@gmail.com>
2022-10-16 23:28:19 -0500

    updated i2c extender module and testbench

--- src/hdl/i2c_extender.v
+++ src/hdl/i2c_extender.v
@@ -2,11 +2,22 @@
 //
 //
 module i2c_extender (
-input sda,
-input scl,
-output scl,
-output sda1,
-output sda2,
+  input scl_in,
+  input sda_in,
+  output reg scl_out,
+  output reg sda_out1,
+  output reg sda_out2
 );
+//wire scl_in;
+//wire sda_in;
+//reg scl_out
+//reg sda_out1;
+//reg sda_out2;
+
+  always @(posedge scl_in) begin
+    scl_out = scl_in;
+    sda_out1 = sda_in;
+    sda_out2 = 0;
+  end
 
 endmodule
--- test/i2c_extender_tb.sv
+++ test/i2c_extender_tb.sv
@@ -1,16 +1,24 @@
 // Testbench for i2c_extender 
-//
+`timescale 1 ns/10 ps
+`include "../src/hdl/i2c_extender.v"
 
-class transaction;
-  // declaring transaction items
-  bit scl;
-  bit sda;
+module i2c_exdenter_tb_top;
+  reg scl_in;
+  wire sda_in;
+  reg scl_out;
+  reg sda_out1;
+  reg sda_out2;
+  i2c_extender UUT(sda_in, scl_in, scl_out, sda_out1, sda_out2); 
+  integer i;
+  initial begin
+    $dumpfile("testbench.vcd");
+    $dumpvars(0,scl_in, scl_out);
+    scl_in = 0;
+    #10
 
-function void display(string name);
-  $display("----------------------------");
-  $display("- %s", name);
-  $display("----------------------------");
-  $display("- scl = %0d, sda = %0d", scl, sda);
-  $display("----------------------------");
-endfunction
-endclass
+    for (i=0; i<10; i++) begin 
+      #10 scl_in = ~scl_in;
+    end
+    end
+
+    endmodule
