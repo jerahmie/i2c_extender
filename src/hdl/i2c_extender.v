b65865881e7ebf5078dcd0e0d78253f5d33c4ce9
Jerahmie Radder <jerahmie@gmail.com>
2022-11-25 13:47:14 -0600

    added output select and expanded to 8 ouputs

--- src/hdl/i2c_extender.v
+++ src/hdl/i2c_extender.v
@@ -1,23 +1,129 @@
 // i2c_extender module
 //
+//      +----------------------+
+//    --| scl_in      sda_out0 |--
+//    --| sda_in      scl_out0 |--
+//      |                      |
+//      |             sda_out1 |--
+//      |             scl_out1 |--
+//      +----------------------+
 //
-module i2c_extender (
+  module i2c_extender (
   input wire scl_in,
   input wire sda_in,
+  input wire [2:0] sel,
   output reg scl_out,
+  output reg sda_out0,
   output reg sda_out1,
-  output reg sda_out2
+  output reg sda_out2,
+  output reg sda_out3,
+  output reg sda_out4,
+  output reg sda_out5,
+  output reg sda_out6,
+  output reg sda_out7
 );
-//wire scl_in;
-//wire sda_in;
-//reg scl_out
-//reg sda_out1;
-//reg sda_out2;
 
   always @(scl_in) begin
     scl_out <= scl_in;
-    sda_out1 <= sda_in;
-    sda_out2 <= 0;
+    if ( sel == 0 )
+    begin
+      sda_out0 <= sda_in;
+      sda_out1 <= 0;
+      sda_out2 <= 0;
+      sda_out3 <= 0;
+      sda_out4 <= 0;
+      sda_out5 <= 0;
+      sda_out6 <= 0;
+      sda_out7 <= 0;
+    end
+    else if ( sel == 1 )
+    begin
+      sda_out0 <= 0;
+      sda_out1 <= sda_in;
+      sda_out2 <= 0;
+      sda_out3 <= 0;
+      sda_out4 <= 0;
+      sda_out5 <= 0;
+      sda_out6 <= 0;
+      sda_out7 <= 0;
+    end
+    else if ( sel == 2 )
+    begin
+      sda_out0 <= 0;
+      sda_out1 <= 0;
+      sda_out2 <= sda_in;
+      sda_out3 <= 0;
+      sda_out4 <= 0;
+      sda_out5 <= 0;
+      sda_out6 <= 0;
+      sda_out7 <= 0;
+    end
+    else if ( sel == 3 )
+    begin
+      sda_out0 <= 0;
+      sda_out1 <= 0;
+      sda_out2 <= 0;
+      sda_out3 <= sda_in;
+      sda_out4 <= 0;
+      sda_out5 <= 0;
+      sda_out6 <= 0;
+      sda_out7 <= 0;
+    end
+    else if ( sel == 4 )
+    begin
+      sda_out0 <= 0;
+      sda_out1 <= 0;
+      sda_out2 <= 0;
+      sda_out3 <= 0;
+      sda_out4 <= sda_in;
+      sda_out5 <= 0;
+      sda_out6 <= 0;
+      sda_out7 <= 0;
+    end
+    else if ( sel == 5 )
+    begin
+      sda_out0 <= 0;
+      sda_out1 <= 0;
+      sda_out2 <= 0;
+      sda_out3 <= 0;
+      sda_out4 <= 0;
+      sda_out5 <= sda_in;
+      sda_out6 <= 0;
+      sda_out7 <= 0;
+    end
+    else if ( sel == 6 )
+    begin
+      sda_out0 <= 0;
+      sda_out1 <= 0;
+      sda_out2 <= 0;
+      sda_out3 <= 0;
+      sda_out4 <= 0;
+      sda_out5 <= 0;
+      sda_out6 <= sda_in;
+      sda_out7 <= 0;
+    end
+    else if ( sel == 7 )
+    begin
+      sda_out0 <= 0;
+      sda_out1 <= 0;
+      sda_out2 <= 0;
+      sda_out3 <= 0;
+      sda_out4 <= 0;
+      sda_out5 <= 0;
+      sda_out6 <= 0;
+      sda_out7 <= sda_in;
+    end
+    else   // should not happen
+    begin
+      sda_out0 <= 0;
+      sda_out1 <= 0;
+      sda_out2 <= 0;
+      sda_out3 <= 0;
+      sda_out4 <= 0;
+      sda_out5 <= 0;
+      sda_out6 <= 0;
+      sda_out7 <= 0;
+    end
   end
 
 endmodule
--- test/i2c_extender_tb.sv
+++ test/i2c_extender_tb.sv
@@ -5,16 +5,27 @@
 module i2c_exdenter_tb_top;
   reg scl_in;
   reg sda_in;
+  reg [2:0] sel; 
   reg scl_out;
   reg sda_out1;
   reg sda_out2;
-  i2c_extender UUT(scl_in, sda_in, scl_out, sda_out1, sda_out2); 
+  reg sda_out3;
+  reg sda_out4;
+  reg sda_out5;
+  reg sda_out6;
+  reg sda_out7;
+  reg sda_out8;
+
+  i2c_extender UUT(scl_in, sda_in, sel, scl_out, sda_out1, sda_out2,
+                   sda_out3, sda_out4, sda_out5, sda_out6, 
+                   sda_out7, sda_out8); 
   integer i;
   initial begin
     $dumpfile("testbench.vcd");
     $dumpvars(0, i2c_exdenter_tb_top);
     scl_in = 0;
     sda_in = 0;
+    sel = 0; 
     #10
 
     for (i=0; i<10; i++) begin 
