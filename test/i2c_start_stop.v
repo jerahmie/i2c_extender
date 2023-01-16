f62d2890ca1084bf5df922b1aff7c6ee1e71487b
Jerahmie Radder <jerahmie@gmail.com>
2023-01-16 17:11:24 -0600

    successfully reading address

--- test/i2c_start_stop.v
+++ test/i2c_start_stop.v
@@ -1,70 +1,104 @@
 // i2c_start_stop
 // example module to test the FSM concept for i2c start/stop condictions
 //
-//
-module i2c_start_stop(
-  input wire sda,
-  input wire scl,
-  input wire clk,
-  input wire reset
-);
-  
-  reg [7:0] data_frame_buf = 0;
-  reg rw_buf = 0;
-  reg [2:0] data_frame_count = 3'b000;
-  localparam [1:0]  // fsm states
-   fsm_wait = 2'b00,
-   fsm_start1 = 2'b01,
-   fsm_start = 2'b10,
-   fsm_read = 2'b11;
 
-   reg [1:0] i2c_state, i2c_state_next;
+//`define TRISTATE_SUPPORT 
+
+`ifdef  TRISTATE_SUPPORT
+  `define HIGHZ 1'bz
+`else
+  `define HIGHZ 1'b1  // change to 1'bZ for implementation
+`endif
+
+module i2c_start_stop(
+  inout sda,
+  input wire scl,
+  input wire clk, input wire reset);
+
+ reg sda_oe;
+ reg sda_out;
+ reg sda_in;
+
+  assign sda  = (sda_oe) ? sda_out: `HIGHZ;
+  //assign sda_oe = 0;
+  assign sda_out = 1;
+  assign sda_in = sda;
+
+  reg [7:0] data_frame_buf;
+  reg [2:0] data_frame_count;
+  localparam [3:0]  // fsm states
+   fsm_wait = 4'b0000,
+   fsm_start1 = 4'b0001,
+   fsm_start = 4'b0010,
+   fsm_read = 4'b0011,
+   fsm_read_wait = 4'b0100,
+   fsm_ack1 = 4'b0101,
+   fsm_ack2 = 4'b0110;
+
+
+   reg [2:0] i2c_state, i2c_state_next;
 
   always @(posedge clk, posedge reset) begin
     if (reset == 1) begin
-      i2c_state = fsm_wait;
-      i2c_state_next = fsm_wait;
-      data_frame_count = 0;
+      i2c_state <= fsm_wait;
+      i2c_state_next <= fsm_wait;
+      data_frame_count <= 3'b000;
+      sda_oe <= 1'b0;
     end
     else begin
-      i2c_state = i2c_state_next;
-    end
-  end
-
-  always @(i2c_state, scl, sda) begin
-    case(i2c_state) 
-      fsm_wait: 
-        if ((!sda) && (scl)) begin
-          i2c_state_next = fsm_start1;
-        end
-      fsm_start1:
+      case(i2c_state) 
+       fsm_wait: begin
+          if ((!sda) && (scl)) begin
+             i2c_state_next <= fsm_start1;
+          end
+         end // fsm_wait
+       fsm_start1: begin
         // sda pulled low and scl high
-        if ((!sda) && (!scl)) begin
-          i2c_state_next = fsm_start;
-        end
-      fsm_start: begin
+         if (sda) begin
+           i2c_state_next <= fsm_wait;
+         end
+         if ((!sda) && (!scl)) begin
+            i2c_state_next <= fsm_start;
+         end
+       end // fsm_start1
+       fsm_start: begin
         // scl pulled low after sda
-        i2c_state_next = fsm_read;
-        data_frame_count = 0;
-      end
-      fsm_read: begin
+        i2c_state_next <= fsm_read;
+        //data_frame_count <= 3'b000;
+       end // fsm_start
+       fsm_read: begin
         // read address and r/w bit
         if (scl) begin
-          if (data_frame_count < 3'b111) begin
-          data_frame_buf[data_frame_count] = sda;
-          data_frame_count = data_frame_count + 3'b1;
-        //end else if ((scl) && (data_frame_count == 8)) begin
-        //  rw_buf = sda;
-        //  data_frame_count = data_frame_count + 1;
-        //  i2c_state_next = i2c_state;
+          if ((i2c_state == fsm_read) && (i2c_state_next == fsm_read_wait)) begin
+            data_frame_count <= data_frame_count + 3'b001;
           end
+          if (data_frame_count == 3'b111) begin
+            i2c_state_next <= fsm_ack1;
+          end else begin
+            data_frame_buf[data_frame_count] <= sda;
+            i2c_state_next <= fsm_read_wait;
+          end
+        end 
+      end // fsm_read
+      fsm_read_wait: begin
+        if (!scl) begin
+          i2c_state_next <= fsm_read;
         end
-        i2c_state_next = i2c_state;
-      end
+      end // fsm_read_wait
+      fsm_ack1: begin
+        sda_oe <= 1'b1;
+        i2c_state_next <= fsm_ack2;
+      end // fsm_ack1
+      fsm_ack2: begin
+        sda_out <= 1'b1;
+        i2c_state_next <= i2c_state; // hold at current state
+      end // i2c_ack2
       default:
-        i2c_state_next = i2c_state;
+        i2c_state_next <= i2c_state;
     endcase
-
+    i2c_state <= i2c_state_next;
   end
+//
+  end // always(..)
 
 endmodule
---
+++ test/tb_top_i2c_start_stop.cpp
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <iostream>
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vi2c_start_stop.h"
+
+#define MAX_SIM_TIME 500
+vluint64_t sim_time = 0;
+
+// i2c_start condition
+//
+void i2c_start(const int posedge_count, Vi2c_start_stop* dut) {
+  static int count = 0;
+  dut->sda = 0;
+  if (count < 10) {
+    count++;
+  }
+  else
+  {
+    count = 0;
+    dut->scl = 0;
+  }
+}
+void i2c_send_data(const int data, Vi2c_start_stop* dut) {
+  std::cout << "i2c_send_data\n";
+  static int data_count = 1;
+  std::cout << "data_count: " << data_count << '\n';
+  if (dut->scl == 1) {
+    if (data_count < 128) {
+      dut->sda = (data & data_count)? 1: 0;
+      data_count*=2;
+      std::cout << "data_count, sda: " << data_count << ',' << dut->sda << '\n';
+    } else {
+      data_count = 1;
+   }
+  }  
+}
+
+int main(int argc, char** argv, char **env)
+{ 
+  std::cout << "Initializing DUT...\n";
+  Vi2c_start_stop *dut = new Vi2c_start_stop;
+  
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *m_trace = new VerilatedVcdC;
+  dut->trace(m_trace, 5);
+  m_trace->open("waveform.vcd");
+
+  // initialize testbench variables
+  long posedge_count = 0;
+  int data = 0b0111111;
+  dut->reset = 0;
+  dut->clk = 0;
+  dut->scl = 0;
+  dut->sda = 0;  // sda is inout
+
+  while (sim_time < MAX_SIM_TIME) {
+    dut->clk ^= 1;
+    if (dut->clk == 1) {
+      posedge_count++;
+    }
+    if (posedge_count == 10) {
+      dut->reset = 1;
+      dut->scl = 1;
+      dut->sda = 1;
+    }
+    if (posedge_count == 20) {
+      dut->reset = 0;
+    }
+    if (posedge_count >= 40 and posedge_count < 50) {
+      i2c_start(posedge_count, dut);
+    }
+    if (posedge_count >= 60 and posedge_count < 250) {
+      if ((posedge_count % 10 == 0) and (dut->clk == 1)) {
+        dut->scl ^= 1;
+        i2c_send_data(data, dut);  
+      }
+    }
+    dut->eval();
+    m_trace->dump(sim_time);
+    sim_time++;
+  }
+
+  std::cout << "Cleaning up...\n";
+  m_trace->close();
+  delete dut;
+  std::cout << "Done.\n";
+  exit(EXIT_SUCCESS);
+}
